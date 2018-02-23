8c80f9f031dbfd0b44833b517f29b41bddf8e3a4
J. Whitaker McRae <whitaker@jwhitakermcrae.com>
2018-02-23 12:42:51 -0500

    Adding prints and sleeps to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Start Unicorn HAT
+echo "Starting Unicorn HAT ..."
 python /app/unicorn_hat.py &
+sleep 1
 # Start REST API
+echo "Starting REST API ..."
 python /app/rest_api.py &
+sleep 1
 # Print success message
 echo "Successfully started app!"
