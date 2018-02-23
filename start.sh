f2ee7196dc7732564d84a307ca59fc3ba84582b6
J. Whitaker McRae <whitaker@jwhitakermcrae.com>
2018-02-23 12:57:25 -0500

    Removing & after rest api call

--- start.sh
+++ start.sh
@@ -2,10 +2,6 @@
 # Start Unicorn HAT
 echo "Starting Unicorn HAT ..."
 python /app/unicorn_hat.py &
-sleep 1
 # Start REST API
 echo "Starting REST API ..."
-python /app/rest_api.py &
-sleep 1
-# Print success message
-echo "Successfully started app!"
+python /app/rest_api.py
